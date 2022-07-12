d1c6d0561d185f82f7d2722e676cf3bd0772a2c8
MiNombreEsPetros <57458458+MiNombreEsPetros@users.noreply.github.com>
2022-07-12 13:46:27 +0300

    Select functionality

---
+++ js/javascript.js
@@ -0,0 +1,63 @@
+function textchange() {
+
+   if (document.getElementById("new").value == "") {
+      document.getElementById("new").value = "Lesson 1";
+   } else if (document.getElementById("new").value == "Lesson 1") {
+      document.getElementById("new").value = "Lesson 2";
+   } else if (document.getElementById("new").value == "Lesson 2") {
+      document.getElementById("new").value = "Lesson 3";
+   } else if (document.getElementById("new").value == "Lesson 3") {
+      document.getElementById("new").value = "Lesson 4";
+   } else if (document.getElementById("new").value == "Lesson 4") {
+      document.getElementById("new").value = "Lesson 1";
+   }
+
+}
+
+function textchange2() {
+
+   if (document.getElementById("new").value == "") {
+      document.getElementById("new").value = "Lesson 4";
+   } else if (document.getElementById("new").value == "Lesson 1") {
+      document.getElementById("new").value = "Lesson 4";
+   } else if (document.getElementById("new").value == "Lesson 2") {
+      document.getElementById("new").value = "Lesson 1";
+   } else if (document.getElementById("new").value == "Lesson 3") {
+      document.getElementById("new").value = "Lesson 2";
+   } else if (document.getElementById("new").value == "Lesson 4") {
+      document.getElementById("new").value = "Lesson 3";
+   }
+
+}
+
+function textchange3() {
+
+   if (document.getElementById("new1").value == "") {
+      document.getElementById("new1").value = "Example 1";
+   } else if (document.getElementById("new1").value == "Example 1") {
+      document.getElementById("new1").value = "Example 2";
+   } else if (document.getElementById("new1").value == "Example 2") {
+      document.getElementById("new1").value = "Example 3";
+   } else if (document.getElementById("new1").value == "Example 3") {
+      document.getElementById("new1").value = "Example 4";
+   } else if (document.getElementById("new1").value == "Example 4") {
+      document.getElementById("new1").value = "Example 1";
+   }
+
+}
+
+function textchange4() {
+
+   if (document.getElementById("new1").value == "") {
+      document.getElementById("new1").value = "Example 4";
+   } else if (document.getElementById("new1").value == "Example 1") {
+      document.getElementById("new1").value = "Example 4";
+   } else if (document.getElementById("new1").value == "Example 2") {
+      document.getElementById("new1").value = "Example 1";
+   } else if (document.getElementById("new1").value == "Example 3") {
+      document.getElementById("new1").value = "Example 2";
+   } else if (document.getElementById("new1").value == "Example 4") {
+      document.getElementById("new1").value = "Example 3";
+   }
+
+}
